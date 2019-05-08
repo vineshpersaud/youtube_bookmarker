171a3d88fa0251f4454818258ee908ef38607a74
github username <github email address>
2019-05-08 12:38:56 -0400

    create video mvc

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'videos/index'
+  get 'videos/new'
+  get 'videos/crete'
+  get 'videos/edit'
+  get 'videos/update'
+  get 'videos/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
