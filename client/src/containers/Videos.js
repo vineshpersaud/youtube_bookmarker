b826a4e605bdde1e7223f958022733a1c06c1727
github username <github email address>
2019-05-09 11:53:14 -0400

    create videos container react showing in view

---
+++ client/src/containers/Videos.js
@@ -0,0 +1,15 @@
+import React,{Component} from 'react';
+
+class Videos extends Component {
+
+
+
+render(){
+  return(
+    <div className="VideosContainer">
+      <h1>Videos</h1>
+    </div>
+  )}
+}
+
+export default Videos;
