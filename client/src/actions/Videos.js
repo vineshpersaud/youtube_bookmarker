dcd534d7f173cf6749e52bc94fe0665053d4bf68
github username <github email address>
2019-05-09 13:32:50 -0400

    create videos actions and reducer file

---
+++ client/src/actions/Videos.js
@@ -0,0 +1,15 @@
+const setVideos = videos => {
+    return {
+      type: 'GET_VIDEOS_SUCESS',
+      videos
+    }
+}
+
+export const getVideos = () = {
+  return dispatch =>{
+    return fetch(`http://localhost:3001/videos`)
+    .then(response => response.json())
+    .then(videos => dispatch(setVideos(videos)))
+    .catch(error => console.log(error))    
+  }
+}
